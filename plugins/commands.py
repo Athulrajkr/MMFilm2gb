de4578a53b26868abfb3c0c467c0f8a1e5b3ee40
AKR <98525798+Akrtechi@users.noreply.github.com>
2022-08-10 15:49:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -48,7 +48,7 @@ async def start(client, message):
                         InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'https://t.me/auto_m4_mallumovies_bot?startgroup=true')
                     ],[
                         InlineKeyboardButton('📌 MY GROUP', url='https://t.me/MM_ARCHIVES'),
-                        InlineKeyboardButton(⚒ MY OWNER', url='https://t.me/MOVIE_MEADIA')
+                        InlineKeyboardButton('⚒ MY OWNER', url='https://t.me/MOVIE_MEADIA')
                     ],[
                         InlineKeyboardButton('⚠️ HELP', callback_data='help'),
                         InlineKeyboardButton('⚙ ABOUT', callback_data='about')
